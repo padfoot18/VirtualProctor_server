a0e99f463d44dda39440915f67157bd965375d04
Tanay Raul <tanay.raul@somaiya.edu>
2019-03-30 13:42:56 +0530

    removed unnecessary files

--- common/handle_notifications.py
+++
@@ -1,7 +0,0 @@
-from config import fcm_config
-from pyfcm import FCMNotification
-
-
-def send_notification_all(message_title, message_body):
-    push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title,
-                                         message_body=message_body)
