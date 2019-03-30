0ec9861253a8564703f31583871a90b482b0af28
Harsh Patel <harsh.patel4@somaiya.edu>
2019-03-30 10:18:28 +0530

    initial setup

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello World!'
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
