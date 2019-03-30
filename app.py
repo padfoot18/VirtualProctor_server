dc8562a142fb0c25b67b4439606ed745337cc3d7
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-30 10:56:48 +0530

    Login function added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    print(username)
+    print(password)
+    
+    if username == "abcd" and password == "1234":
+        return "true"
+    if username != "abcd" and password != "1234":
+        return "both_false"
+    elif username != "abcd":
+        return "username_false"
+    elif password != "1234":
+        return "password_false"
 
 
 if __name__ == '__main__':
